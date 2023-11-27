c98d78d1f1cc5f64b5ef7e54226c1390f7740d48
shirator <carlito.elm78@gmail.com>
2023-11-27 14:45:18 -0400

    adicionando tempering e rotas

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Awakening from "./assets/components/Awakening.tsx";
 import Tempering from "./assets/components/Tempering.tsx";
