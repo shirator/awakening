92f616c72b1008f6c658bbfa238e8adf0d738a3f
shirator <carlito.elm78@gmail.com>
2023-11-28 16:33:12 -0400

    mudando botao

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,19 @@ import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="flex gap-5 uppercase  text-4xl">
-      <Link to={`awakening`}>Awakening</Link>
-      <Link to={`tempering`}>Tempering</Link>
+    <div className="flex gap-5 uppercase text-4xl">
+      <Link
+        to={`awakening`}
+        className="text-blue-500 hover:text-blue-700 transition duration-300"
+      >
+        Awakening
+      </Link>
+      <Link
+        to={`tempering`}
+        className="text-green-500 hover:text-green-700 transition duration-300"
+      >
+        Tempering
+      </Link>
     </div>
   );
 }
